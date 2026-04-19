26c62c5c462d7cae06a62e900b9315573a505ff2
patrick <patrick22354@gmail.com>
2026-04-19 11:09:36 +0100

    fix: clean next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
